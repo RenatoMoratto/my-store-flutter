8300b8fe952e8ed5228ed6f3770e073f9c80b120
RenatoMoratto <renatowmoratto@gmail.com>
2022-06-29 16:30:55 -0300

    feat: add item to cart

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/badge.dart';
 
 import '../components/product_grid.dart';
+import '../models/cart.dart';
 
 enum FilterOptions {
   favorite,
@@ -43,6 +46,16 @@ class _ProductsOverviewPageState extends State<ProductsOverviewPage> {
                 }
               });
             },
+          ),
+          Consumer<Cart>(
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.shopping_cart),
+            ),
+            builder: (context, cart, child) => Badge(
+              value: cart.itemsCount.toString(),
+              child: child!,
+            ),
           )
         ],
       ),
