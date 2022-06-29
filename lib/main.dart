dd68723315b02f1cace0649c5eecd95437cb7e77
RenatoMoratto <renatowmoratto@gmail.com>
2022-06-29 17:01:55 -0300

    feat: create cart page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/pages/cart_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
 
@@ -33,6 +34,7 @@ class MyApp extends StatelessWidget {
         home: const ProductsOverviewPage(),
         routes: {
           AppRoutes.productDetail: (context) => const ProductDetailPage(),
+          AppRoutes.cart: (context) => const CartPage(),
         },
         debugShowCheckedModeBanner: false,
       ),
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/badge.dart';
+import 'package:shop/utils/app_routes.dart';
 
 import '../components/product_grid.dart';
 import '../models/cart.dart';
@@ -49,7 +50,9 @@ class _ProductsOverviewPageState extends State<ProductsOverviewPage> {
           ),
           Consumer<Cart>(
             child: IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context).pushNamed(AppRoutes.cart);
+              },
               icon: const Icon(Icons.shopping_cart),
             ),
             builder: (context, cart, child) => Badge(
