1e76c38170d7aac70582072cadc034870fc1cd45
RenatoMoratto <renatowmoratto@gmail.com>
2022-06-29 10:40:31 -0300

    feat: product overview page with mock data

---
+++ lib/components/product_item.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+  const ProductItem({Key? key, required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.favorite),
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_cart),
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+        ),
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
