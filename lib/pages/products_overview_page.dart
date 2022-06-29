3bd63710823e66570af964aa091fe55602fb15fc
RenatoMoratto <renatowmoratto@gmail.com>
2022-06-29 15:18:46 -0300

    feat: add favorite filter

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 
 import '../components/product_grid.dart';
+
+enum FilterOptions {
+  favorite,
+  all,
+}
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Minha Loja')),
+      appBar: AppBar(
+        title: const Text('Minha Loja'),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: FilterOptions.favorite,
+                child: Text('Somente favoritos'),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.all,
+                child: Text('Todos'),
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.favorite) {
+                provider.showFavoriteOnly();
+              } else {
+                provider.showAll();
+              }
+            },
+          )
+        ],
+      ),
       body: const ProductGrid(),
     );
   }
