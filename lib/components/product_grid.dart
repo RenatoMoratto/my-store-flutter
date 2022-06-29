eab7e35a413c20aaf9b251c0db66c34c3d07f6fd
RenatoMoratto <renatowmoratto@gmail.com>
2022-06-29 12:28:54 -0300

    refactor: move loadedProducts instantiation to productGrid

--- lib/components/product_grid.dart
+++ lib/components/product_grid.dart
@@ -2,18 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../models/product.dart';
+import '../models/product_list.dart';
 import 'product_item.dart';
 
 class ProductGrid extends StatelessWidget {
-  const ProductGrid({
-    Key? key,
-    required this.loadedProducts,
-  }) : super(key: key);
-
-  final List<Product> loadedProducts;
+  const ProductGrid({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+    final List<Product> loadedProducts = provider.items;
+
     return GridView.builder(
       padding: const EdgeInsets.all(10),
       itemCount: loadedProducts.length,
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import '../components/product_grid.dart';
-import '../models/product.dart';
-import '../models/product_list.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadedProducts = provider.items;
-
     return Scaffold(
       appBar: AppBar(title: const Text('Minha Loja')),
-      body: ProductGrid(loadedProducts: loadedProducts),
+      body: const ProductGrid(),
     );
   }
 }
