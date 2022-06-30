7bf8a70569195faffee2d27ff70062cd6b6b1d8c
RenatoMoratto <renatowmoratto@gmail.com>
2022-06-30 16:29:37 -0300

    feat: remove product option

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/utils/app_routes.dart';
 
 import '../models/product.dart';
+import '../models/product_list.dart';
 
 class ProductItem extends StatelessWidget {
   final Product product;
@@ -30,7 +32,30 @@ class ProductItem extends StatelessWidget {
               icon: const Icon(Icons.edit),
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () => showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: const Text('Excluir Produto'),
+                  content: const Text('Quer remover o produto da lista?'),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: const Text('Não'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Provider.of<ProductList>(
+                          context,
+                          listen: false,
+                        ).removeProduct(product);
+
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Sim'),
+                    ),
+                  ],
+                ),
+              ),
               color: Theme.of(context).errorColor,
               icon: const Icon(Icons.delete),
             ),
