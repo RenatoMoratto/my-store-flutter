8f784a6c02df0ea59cc7acab5524b9f22e55e5c8
RenatoMoratto <renatowmoratto@gmail.com>
2022-06-30 16:13:51 -0300

    feat: allow product edit

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/utils/app_routes.dart';
 
 import '../models/product.dart';
 
@@ -19,7 +20,12 @@ class ProductItem extends StatelessWidget {
         child: Row(
           children: [
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context).pushNamed(
+                  AppRoutes.productForm,
+                  arguments: product,
+                );
+              },
               color: Theme.of(context).primaryColor,
               icon: const Icon(Icons.edit),
             ),
