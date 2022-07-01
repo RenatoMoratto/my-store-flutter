cd4bb5460810e671a8a93355955e4ff66963453d
RenatoMoratto <renatowmoratto@gmail.com>
2022-07-01 12:41:29 -0300

    feat: update products from firebase realtime database

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -8,8 +8,8 @@ import 'product.dart';
 
 class ProductList with ChangeNotifier {
   final List<Product> _items = [];
-  final String _url =
-      'https://my-store-flutter-default-rtdb.firebaseio.com/products.json';
+  final String _baseUrl =
+      'https://my-store-flutter-default-rtdb.firebaseio.com/products';
 
   List<Product> get items => [..._items];
 
@@ -21,7 +21,9 @@ class ProductList with ChangeNotifier {
   Future<void> loadProducts() async {
     _items.clear();
 
-    final response = await http.get(Uri.parse(_url));
+    final response = await http.get(
+      Uri.parse('$_baseUrl.json'),
+    );
 
     if (response.body == 'null') return;
 
@@ -61,7 +63,7 @@ class ProductList with ChangeNotifier {
 
   Future<void> addProduct(Product product) async {
     final response = await http.post(
-      Uri.parse(_url),
+      Uri.parse('$_baseUrl.json'),
       body: jsonEncode(
         {
           "name": product.name,
@@ -84,10 +86,22 @@ class ProductList with ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> updateProduct(Product product) {
+  Future<void> updateProduct(Product product) async {
     int index = _items.indexWhere((p) => p.id == product.id);
 
     if (index >= 0) {
+      await http.patch(
+        Uri.parse('$_baseUrl/${product.id}.json'),
+        body: jsonEncode(
+          {
+            "name": product.name,
+            "description": product.description,
+            "price": product.price,
+            "imageUrl": product.imageUrl,
+          },
+        ),
+      );
+
       _items[index] = product;
       notifyListeners();
     }
