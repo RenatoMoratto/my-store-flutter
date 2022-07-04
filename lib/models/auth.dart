d1e4e8ab115d01b7d26cfc3cb62d5cc4afb6b14e
RenatoMoratto <renatowmoratto@gmail.com>
2022-07-04 12:50:27 -0300

    feat: error treatment on authentication

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop/exceptions/auth_exception.dart';
 
 class Auth with ChangeNotifier {
   Future<void> _authenticate(
@@ -19,14 +20,18 @@ class Auth with ChangeNotifier {
         'returnSecureToken': true,
       }),
     );
-    final data = jsonDecode(response.body);
+    final body = jsonDecode(response.body);
+
+    if (body['error'] != null) {
+      throw AuthException(body['error']['message']);
+    }
   }
 
   Future<void> signup(String email, String password) async {
-    _authenticate(email, password, 'signUp');
+    return _authenticate(email, password, 'signUp');
   }
 
   Future<void> login(String email, String password) async {
-    _authenticate(email, password, 'signInWithPassword');
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
