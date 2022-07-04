6a7747fe61929fb26deb24ca0d987057ea531254
RenatoMoratto <renatowmoratto@gmail.com>
2022-07-04 12:26:41 -0300

    feat: login with firebase

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -4,19 +4,29 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Auth with ChangeNotifier {
-  static const _url =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDyYNasI5swNJWxrG2StDMwO4ZF9L2RrU4';
-
-  Future<void> signup(String email, String password) async {
+  Future<void> _authenticate(
+    String email,
+    String password,
+    String urlFragment,
+  ) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlFragment?key=AIzaSyDyYNasI5swNJWxrG2StDMwO4ZF9L2RrU4';
     final response = await http.post(
-      Uri.parse(_url),
+      Uri.parse(url),
       body: jsonEncode({
         'email': email,
         'password': password,
         'returnSecureToken': true,
       }),
     );
-
     final data = jsonDecode(response.body);
+  }
+
+  Future<void> signup(String email, String password) async {
+    _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    _authenticate(email, password, 'signInWithPassword');
   }
 }
