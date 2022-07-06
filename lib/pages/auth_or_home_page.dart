0ee7d07a34e677d5cb8cd1dc95117e1974648390
RenatoMoratto <renatowmoratto@gmail.com>
2022-07-06 09:42:19 -0300

    feat: persist user data

--- lib/pages/auth_or_home_page.dart
+++ lib/pages/auth_or_home_page.dart
@@ -11,6 +11,17 @@ class AuthOrHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Auth auth = Provider.of(context);
-    return auth.isAuth ? const ProductsOverviewPage() : const AuthPage();
+    return FutureBuilder(
+      future: auth.tryAutoLogin(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.error != null) {
+          return const Center(child: Text('Ocorreu um erro!'));
+        }
+        return auth.isAuth ? const ProductsOverviewPage() : const AuthPage();
+      },
+    );
   }
 }
